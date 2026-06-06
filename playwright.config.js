41b819655a72ed28696287764432905f8ca1db2f
Adriano Costa de Queiroz <adriano-costa.queiroz@hotmail.com>
2026-06-06 14:33:15 -0300

    ci: ajustando relatorio

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
     ['html', { open: false }],
     ['junit', { outputFile: `results.xml` }],
     ['list'],
-    ['playwright-ctrf-json-reporter', { outputFile: 'ctrf/report.json' }]
+    ['playwright-ctrf-json-reporter', { outputFile: 'ctrf/report.json' }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
